60d0220390ed6dfeef835fd8f794c8c72e0ef756
Damian-Gluszynski <71399957+Damian-Gluszynski@users.noreply.github.com>
2020-10-01 12:49:20 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,3 @@
-basic.showLeds(`
-    . . . . .
-    . . . # .
-    . . . . .
-    . . . # .
-    . . . . .
-    `)
 basic.forever(function () {
-    robotbit.Servo(robotbit.Servos.S1, 0)
-    basic.pause(1000)
     robotbit.Servo(robotbit.Servos.S1, 180)
-    basic.pause(1000)
 })
